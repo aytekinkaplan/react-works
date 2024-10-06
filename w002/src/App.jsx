7feddbb0b519083df5f51e8098ae7fb1f38b77e4
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-06 17:34:07 +0300

    Up

--- w002/src/App.jsx
+++ w002/src/App.jsx
@@ -1,19 +1,8 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
